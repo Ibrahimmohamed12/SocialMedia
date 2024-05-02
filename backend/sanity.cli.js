7409ca849a8e4c9e7b8db31776a6d5f823fe65b6
Ibrahimmohamed12 <78480722+Ibrahimmohamed12@users.noreply.github.com>
2024-05-02 16:10:24 +0300

    Delete backend directory

--- backend/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'o97t6hjk',
-    dataset: 'production'
-  }
-})
--- backend/schemaTypes/index.js
+++
@@ -1,6 +0,0 @@
-import comment from './comment'
-import postedBy from './postedBy'
-import pin from './pin'
-import save from './save'
-import user from './user'
-export const schemaTypes = [comment,postedBy,pin,save,user]
